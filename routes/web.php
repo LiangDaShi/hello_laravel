c1b5e568f1f2e045e6364a269ff11e2e866c642d
lmaster <407987823@qq.com>
2018-03-14 07:52:43 +0000

    Finish user signup

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
+// resource 方法将遵从 RESTful 架构为用户资源生成路由
+// resource等与下面代码
+// Route::get('/users', 'UsersController@index')->name('users.index');
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+// Route::get('/users/create', 'UsersController@create')->name('users.create');
+// Route::post('/users', 'UsersController@store')->name('users.store');
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+// Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+// 说明
+// GET    /users    UsersController@index    显示所有用户列表的页面
+// GET    /users/{user}    UsersController@show    显示用户个人信息的页面
+// GET    /users/create    UsersController@create    创建用户的页面
+// POST    /users    UsersController@store    创建用户
+// GET    /users/{user}/edit    UsersController@edit    编辑用户个人资料的页面
+// PATCH    /users/{user}    UsersController@update    更新用户
+// DELETE    /users/{user}    UsersController@destroy    删除用户
